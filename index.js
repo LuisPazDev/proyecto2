8899a49eae19865f6f62ad251abb9aa61015f7f2
Luispaz86 <lupaz.dwfsc10@ucamp.utel.edu.mx>
2022-09-20 10:58:26 -0500

    actualizando Index.js

--- index.js
+++ index.js
@@ -1,4 +1,52 @@
 let form = document.getElementById('form');
-let name = document.getElementById('name');
-let comment = document.getElementById('comment');
+
+let inputFieldName = document.getElementById('name');
+let inputFieldEmail = document.getElementById('email');
+let inputFieldComment = document.getElementById('comment');
+
+let submit = document.getElementById('submit');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    if (inputFieldName.value.length < 3){alert('Invalid Name')}
+});
+
+let arrayName = []
+let arrayEmail = []
+let arrayComment = []
+
+let name = ''
+let email = ''
+let comment = ''
+
+inputFieldName.addEventListener('change', function () {
+    name = inputFieldName.value;
+})
+inputFieldEmail.addEventListener('change', function () {
+    email = inputFieldEmail.value;
+});
+inputFieldComment.addEventListener('change', function () {
+    comment = inputFieldComment.value;
+});
+
+submit.addEventListener('click', function () {
+    arrayName.push(name);
+    arrayEmail.push(email);
+    arrayComment.push(comment);
+    
+    localStorage.setItem('names', arrayName);
+    localStorage.setItem('emails', arrayEmail);
+    localStorage.setItem('comments', arrayComment);    
+});
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ localStorage.js
@@ -0,0 +1,11 @@
+localStorage.setItem('miGato', 'Juan');
+
+let cat = localStorage.getItem('miGato');
+console.log('cat: ' + cat);
+
+localStorage.removeItem('miGato');
+
+localStorage.removeItem('nombres');
+
+
+localStorage.clear();
